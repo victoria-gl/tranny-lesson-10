ef533e0c4d629aeb4f1dccf8e0508f7b37fae314
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-12-08 23:10:58 +0000

    Deploying to gh-pages from @ victoria-gl/tranny-lesson-10@ef7b40e6181a2069b8c48319e2bc39b8eed79093 🚀

--- index.8adbd967.js
+++
@@ -1,2 +0,0 @@
-fetch("https://rickandmortyapi.com/api/character").then((function(c){if(!c.ok)throw new Error("")})).catch((function(c){return console.log("catch",c)}));
-//# sourceMappingURL=index.8adbd967.js.map
