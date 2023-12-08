010a97ae06a3cb20a61c6249f232084549af11de
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-12-08 21:41:09 +0100

    tranny

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@
 // https://www.weatherapi.com/docs/
 
 
+
+
+//Fetch
+
+// fetch("https://rickandmortyapi.com/api/character")
+//       .then(response => console.log("then",response))
+//       .catch(err => console.log("catch", err))
+      
+
+fetch("https://rickandmortyapi.com/api/character")
+      .then(data => {
+        if(!data.ok){
+            throw new Error("")
+        }
+      })
+      .catch(err => console.log("catch", err))
