086db1b161d16da672b62bed9e382645a85f114c
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-12-19 13:32:50 +0100

    tranny

--- src/script.js
+++ src/script.js
@@ -151,12 +151,11 @@
 
 // *********************** Кнопка "Load More" ************************** \\
 
-
 const container = document.querySelector(".js-movie-list");
 const loadMore = document.querySelector(".js-load-more");
 let page = 1;
 
-// loadMore.addEventListener("click", onLoadMore)
+loadMore.addEventListener("click", onLoadMore)
 
 serviceMovie()
     .then(data => {
@@ -200,20 +199,73 @@ function createMarkup(arr) {
     `).join("")
 }
 
-function onLoadMore(){
+
+function onLoadMore() {
     page += 1;
 
     serviceMovie(page)
         .then(data => {
             container.insertAdjacentHTML("beforeend", createMarkup(data.results))
 
-            if(data.page >= data.total_pages){
+            if(data.page >= data.total_pages) { // data.page >=
                 loadMore.classList.replace("load-more", "load-more-hidden")
             }
         })
 }
 
+function serviceMovie(page = 1) {
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const END_POINT = "/trending/movie/week";
+    const API_KEY = "345007f9ab440e5b86cef51be6397df1";
 
+    const queryParams = new URLSearchParams({
+        api_key: API_KEY,
+        page: page
+    })
+
+    return axios.get(`${BASE_URL}${END_POINT}?${queryParams}`)
+        .then(resp => {
+            return resp.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+}
+
+async function render() {
+    try {
+        const data = await serviceMovie()
+        container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+
+        if(data.page < data.total_pages) { // data.page < 500
+            loadMore.classList.replace("load-more-hidden", "load-more")
+        }
+    } catch(error) {
+        console.log("Error!!!!", error);
+    }
+}
+render()
+
+async function serviceMovie(page = 1) {
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const END_POINT = "/trending/movie/week";
+    const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+
+    const queryParams = new URLSearchParams({
+        api_key: API_KEY,
+        page: page
+    })
+
+    try {
+        const res = await axios.get(`${BASE_URL}${END_POINT}?${queryParams}`)
+        return await res.data
+    } catch(error) {
+        throw new Error(error)
+    }
+
+}
+
+serviceMovie()
 
 // ********************************Infinity scroll ********************** \\
 
